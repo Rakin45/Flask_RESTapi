4c849ae86ea83e3b93a55510f8cd32af36a12e38
Rakin <rakinahmed45@gmail.com>
2024-02-21 21:18:40 +0000

    models updated to be of standard flask-sqlalchemy format

--- webapp/models.py
+++ webapp/models.py
@@ -1,87 +1,91 @@
-from typing import List
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Integer, String, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
 from webapp import db
-
-class Location(db.Model):
-    __tablename__ = 'locations'
-    
-    location_id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
-    location_name: Mapped[str] = mapped_column(db.String, nullable=False)
-    latitude: Mapped[float] = mapped_column(db.Float, nullable=True)
-    longitude: Mapped[float] = mapped_column(db.Float, nullable=True)
-    water_quality_data: Mapped[List['WaterQualityData']] = relationship('WaterQualityData', back_populates='location')
-    def __repr__(self):
-        return f'<Location {self.location_name}>'
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     __tablename__ = 'users'
+
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False,unique=True)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    full_name = db.Column(db.String, nullable=True)
+    company = db.Column(db.String, nullable=True)
+    profession = db.Column(db.String, nullable=True)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
     
-    user_id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
-    email: Mapped[str] = mapped_column(db.String, unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(db.String, nullable=False)
-    full_name: Mapped[str] = mapped_column(db.String, nullable=True)
-    company: Mapped[str] = mapped_column(db.String, nullable=True)
-    profession: Mapped[str] = mapped_column(db.String, nullable=True)
-    
-    
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+class Location(db.Model):
+    __tablename__ = 'locations'
+
+    location_id = db.Column(db.Integer, primary_key=True)
+    location_name = db.Column(db.String, nullable=False)
+    latitude = db.Column(db.Float, nullable=True)
+    longitude = db.Column(db.Float, nullable=True)
+    water_quality_data = db.relationship('WaterQualityData', back_populates='location')
+
+    def __repr__(self):
+        return f'<Location {self.location_name}>'
+
+
 
 class UploadedData(db.Model):
     __tablename__ = 'uploaded_data'
-    
-    data_id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
-    user_id: Mapped[int] = mapped_column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
-    location_id: Mapped[int] = mapped_column(db.Integer, db.ForeignKey('locations.location_id'), nullable=False)
-    data: Mapped[str] = mapped_column(db.String, nullable=False)
-    
-    # Define relationships with backref to allow for querying from the other side
-    user: Mapped[User] = relationship('User', backref='uploaded_data')
-    location: Mapped[Location] = relationship('Location', backref='uploaded_data')
-    
+
+    data_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    location_id = db.Column(db.Integer, db.ForeignKey('locations.location_id'), nullable=False)
+    data = db.Column(db.String, nullable=False)
+
+    user = db.relationship('User', backref='uploaded_data')
+    location = db.relationship('Location', backref='uploaded_data')
+
     def __repr__(self):
         return f'<UploadedData {self.data_id}>'
 
 class VisualisationData(db.Model):
     __tablename__ = 'visualisation_data'
-    
-    visualisation_id: Mapped[int] = mapped_column(db.Integer, primary_key=True, autoincrement=True)
-    upload_id: Mapped[int] = mapped_column(db.Integer, db.ForeignKey('uploaded_data.data_id'), nullable=False)
-    location_id: Mapped[int] = mapped_column(db.Integer, db.ForeignKey('locations.location_id'), nullable=False)
-    forecast_data: Mapped[str] = mapped_column(db.String, nullable=True)
-    
-    # Relationships must correspond with those defined in UploadedData and Location classes
-    upload: Mapped[UploadedData] = relationship('UploadedData', back_populates='visualisation_data')
-    location: Mapped[Location] = relationship('Location', back_populates='visualisation_data')
+
+    visualisation_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    upload_id = db.Column(db.Integer, db.ForeignKey('uploaded_data.data_id'), nullable=False)
+    location_id = db.Column(db.Integer, db.ForeignKey('locations.location_id'), nullable=False)
+    forecast_data = db.Column(db.String, nullable=True)
+
+    upload = db.relationship('UploadedData', back_populates='visualisation_data')
+    location = db.relationship('Location', back_populates='visualisation_data')
 
 class WaterQualityData(db.Model):
     __tablename__ = 'water_quality_data'
-    
-    id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
-    location_id: Mapped[int] = mapped_column(db.Integer, db.ForeignKey('locations.location_id'), nullable=False)
-    date: Mapped[date] = mapped_column(db.Date, nullable=False)
-    
-    spec_cond_max: Mapped[float] = mapped_column(db.Float, nullable=True)
-    ph_max: Mapped[float] = mapped_column(db.Float, nullable=True)
-    ph_min: Mapped[float] = mapped_column(db.Float, nullable=True)
-    spec_cond_min: Mapped[float] = mapped_column(db.Float, nullable=True)
-    spec_cond_mean: Mapped[float] = mapped_column(db.Float, nullable=True)
-    dissolved_oxy_max: Mapped[float] = mapped_column(db.Float, nullable=True)
-    dissolved_oxy_mean: Mapped[float] = mapped_column(db.Float, nullable=True)
-    dissolved_oxy_min: Mapped[float] = mapped_column(db.Float, nullable=True)
-    temp_mean: Mapped[float] = mapped_column(db.Float, nullable=True)
-    temp_min: Mapped[float] = mapped_column(db.Float, nullable=True)
-    temp_max: Mapped[float] = mapped_column(db.Float, nullable=True)
-    water_quality: Mapped[float] = mapped_column(db.Float, nullable=True)
-    training: Mapped[bool] = mapped_column(db.Boolean, nullable=True)
-    
-    # Relationship to Location
-    location: Mapped['Location'] = relationship('Location', back_populates='water_quality_data')
+
+    id = db.Column(db.Integer, primary_key=True)
+    location_id = db.Column(db.Integer, db.ForeignKey('locations.location_id'), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+
+    spec_cond_max = db.Column(db.Float, nullable=True)
+    ph_max = db.Column(db.Float, nullable=True)
+    ph_min = db.Column(db.Float, nullable=True)
+    spec_cond_min = db.Column(db.Float, nullable=True)
+    spec_cond_mean = db.Column(db.Float, nullable=True)
+    dissolved_oxy_max = db.Column(db.Float, nullable=True)
+    dissolved_oxy_mean = db.Column(db.Float, nullable=True)
+    dissolved_oxy_min = db.Column(db.Float, nullable=True)
+    temp_mean = db.Column(db.Float, nullable=True)
+    temp_min = db.Column(db.Float, nullable=True)
+    temp_max = db.Column(db.Float, nullable=True)
+    water_quality = db.Column(db.Float, nullable=True)
+    training = db.Column(db.Boolean, nullable=True)
+
+    location = db.relationship('Location', back_populates='water_quality_data')
 
     def __repr__(self):
-        return (f'<WaterQualityData id={self.id}, '
-                f'Location ID={self.location_id}, Date={self.date}>')
+        return f'<WaterQualityData id={self.id}, Location ID={self.location_id}, Date={self.date}>'
 
 # Back populates defined outside of classes to avoid circular import issues
-UploadedData.visualisation_data = relationship('VisualisationData', uselist=False, back_populates='upload')
-Location.visualisation_data = relationship('VisualisationData', back_populates='location')
+UploadedData.visualisation_data = db.relationship('VisualisationData', uselist=False, back_populates='upload')
+Location.visualisation_data = db.relationship('VisualisationData', back_populates='location')
